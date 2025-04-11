92305677f5aeea093b2b9048bb23a52eade7b757
人見悠斗 <haruto.77.512@gmail.com>
2025-04-11 13:47:52 +0900

    スケジューリングを複数日で登録させる

--- app/forms.py
+++ app/forms.py
@@ -41,29 +41,29 @@ class BS4ScheduleForm(forms.ModelForm):
             self.fields['start_date'].widget.attrs['class'] = 'form-control'
             self.fields['start_date'].disabled = True  # disabledでフォーム送信時に値を送信しないようにする
 
-    def clean_end_date_time(self):
-        start_time = self.cleaned_data['start_time']
-        end_time = self.cleaned_data['end_time']
-        start_date = self.cleaned_data['start_date']
-        end_date = self.cleaned_data['end_date']
+    # 各フィールドに対してのバリデーション確認
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
+
+        # どれかの日時が未入力の場合
+        if None in (start_date, end_date, start_time, end_time):
+            raise forms.ValidationError('すべての日時を入力してください。')
+
+        # 終了日より開始日の方が後の日付に設定されていた場合
         if end_date < start_date:
-            raise forms.ValidationError(
-                '終了日は、開始日よりも後にしてください'
-            )
-        if end_date == start_date and end_time <= start_time:
-            raise forms.ValidationError(
-                '終了時間は、開始時間よりも後にしてください'
-            )
-        return end_time
-    
-    def clean_end_date(self):
-        start_date = self.cleaned_data['start_date']
-        end_date = self.cleaned_data['end_date']
-        if end_date < start_date:
-            raise forms.ValidationError(
-                '終了日は、開始日よりも後にしてください'
-            )
-        return end_date
+            self.add_error('end_date','終了日が開始日より前に設定されています')
+        
+        if start_date == end_date:
+            # 終了時間と開始時間が同じまたは、開始時間のほうが後に設定されている場合
+            if end_time <= start_time:
+                self.add_error('end_time', '終了時間が開始時間よりも前に設定されています')
+        
+        return cleaned_data
+
 
 
 class SimpleScheduleForm(forms.ModelForm):
